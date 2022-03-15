6d42dad530c526f7dcac345455fde42ddaf17e21
Juan Carlos <jchernandez827@gmail.com>
2022-03-15 15:26:58 -0500

    Add routes, models, and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :groups, dependent: destroy
+  has_many :expenses, foreign_key: 'author_id', class_name: 'Expense', dependent: destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
