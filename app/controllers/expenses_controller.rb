10f96577c59a743c346badd2a88ff4206165ca20
Juan Carlos <jchernandez827@gmail.com>
2022-03-24 21:10:57 -0500

    Now categories displays the correct expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,13 +2,14 @@ class ExpensesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @expenses = Expense.all
+    @expenses = Group.find(params[:group_id]).expenses
   end
 
   def show; end
 
   def new
     @expense = Expense.new
+    @groups = Group.where(author_id: current_user.id)
   end
 
   def create
@@ -16,8 +17,11 @@ class ExpensesController < ApplicationController
     @expense.name = params[:expense][:name]
     @expense.amount = params[:expense][:amount]
     @expense.author_id = current_user.id
+    categories = params[:groups]
     if @expense.save
-      ExpensesGroup.create(expense_id: @expense.id, group_id: params[:group_id])
+      categories.each do |category|
+        ExpensesGroup.create(expense_id: @expense.id, group_id: category)
+      end
       flash[:notice] = 'Transaction created successfully'
       redirect_to group_expenses_path(params[:group_id])
     end
